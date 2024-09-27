2397c660a1cdc96db379a4e72e6bd8a6357e7ed9
ELMANNY4000 <ulodoe@gmail.com>
2024-09-28 00:50:49 +0100

    upadte

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,28 +1,40 @@
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav className="navigation">
         <img src="./images/Logo.svg" alt="" className="logo" />
 
-        <div className="selection-contanier">
-            <div className="cate">
-                <span className="page">Home</span>
-                <span className="page">Service </span>
-                <span className="page">How it  works</span>
-                <span className="page">Testimonials</span>
-            </div>
-            <div className="navigation-btn">
-                <img src="./images/phone.svg" alt="" className="btn-icon" />
-                <span className="btn-text">Book a Call</span>
-            </div>
-            <img src="./images/menu.svg" alt="" className="menu" />
+        <div className={`selection-container ${isMenuOpen ? "open" : ""}`}>
+          <div className="cate">
+            <span className="page">Home</span>
+            <span className="page">Service</span>
+            <span className="page">How it works</span>
+            <span className="page">Testimonials</span>
+          </div>
+          <div className="navigation-btn">
+            <img src="./images/phone.svg" alt="" className="btn-icon" />
+            <span className="btn-text">Book a Call</span>
+          </div>
         </div>
 
+        <img
+          src="./images/menu.svg"
+          alt="menu"
+          className="menu"
+          onClick={toggleMenu}
+        />
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
